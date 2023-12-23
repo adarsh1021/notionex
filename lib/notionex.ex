4f88804f285c424da5c42acbdbbbd43e99ac7d16
Adarsh Menon <adarsh1021@gmail.com>
2023-12-23 22:54:11 +0530

    Context function to block render

--- lib/notionex.ex
+++ lib/notionex.ex
@@ -1,2 +1,11 @@
 defmodule Notionex do
+  alias Notionex.Object
+
+  def render_block(
+        %Object.List{object: "list", type: "block"} = blocks,
+        renderer \\ Notionex.Renderer.HTML.Block,
+        opts \\ %{}
+      ) do
+    apply(renderer, :blocks, [blocks, opts])
+  end
 end
