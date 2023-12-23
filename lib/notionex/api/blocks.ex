3c01fd8f8535629d4564ce93b1e71191251c537c
Adarsh Menon <adarsh1021@gmail.com>
2023-12-23 12:19:12 +0530

    Blocks API retrieve function

--- lib/notionex/api/blocks.ex
+++ lib/notionex/api/blocks.ex
@@ -1,2 +1,16 @@
 defmodule Notionex.API.Blocks do
+  alias Notionex.Client
+  alias Notionex.Object.Block
+
+  @doc """
+  Retrieve a block.
+  """
+  def retrieve(block_id) do
+    %Client.Request{
+      method: :get,
+      url: "blocks/#{block_id}"
+    }
+    |> Client.request!()
+    |> Block.new()
+  end
 end
