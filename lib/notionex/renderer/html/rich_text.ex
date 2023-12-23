72ac6d7054af379315ad667a91ca297a7bee7df1
Adarsh Menon <adarsh1021@gmail.com>
2023-12-23 22:55:25 +0530

    Handle annotations in rich text

--- lib/notionex/renderer/html/rich_text.ex
+++ lib/notionex/renderer/html/rich_text.ex
@@ -9,49 +9,24 @@ defmodule Notionex.Renderer.HTML.RichText do
   end
 
   def text(%{"type" => "text"} = rich_text) do
-    plain_text = Map.get(rich_text, "plain_text")
-    "<p>#{plain_text}</p>"
+    rich_text
+    |> apply_annotations()
   end
 
   def text(_), do: ""
 
-  # defp text(%{link: %{url: url}, content: content}) do
-  #   "<a href=\"#{url}\">#{text(content)}</a>"
-  # end
+  defp apply_annotations(%{"type" => "text", "annotations" => annotations} = rich_text) do
+    content = get_in(rich_text, ["text", "content"])
 
-  # defp text(%{annotations: %{bold: true}, content: content}) do
-  #   "<strong>#{text(content)}</strong>"
-  # end
-
-  # defp text(%{annotations: %{italic: true}, content: content}) do
-  #   "<em>#{text(content)}</em>"
-  # end
-
-  # defp text(%{annotations: %{strikethrough: true}, content: content}) do
-  #   "<del>#{text(content)}</del>"
-  # end
-
-  # defp text(%{annotations: %{underline: true}, content: content}) do
-  #   "<u>#{text(content)}</u>"
-  # end
-
-  # defp text(%{annotations: %{code: true}, content: content}) do
-  #   "<code>#{text(content)}</code>"
-  # end
-
-  # defp text(%{annotations: %{color: color}, content: content}) do
-  #   "<span style=\"color: #{color}\">#{text(content)}</span>"
-  # end
-
-  # defp text(%{annotations: %{color_background: color}, content: content}) do
-  #   "<span style=\"background-color: #{color}\">#{text(content)}</span>"
-  # end
-
-  # defp text(%{annotations: %{color: color, color_background: background}, content: content}) do
-  #   "<span style=\"color: #{color}; background-color: #{background}\">#{text(content)}</span>"
-  # end
-
-  # defp text(%{content: content}) do
-  #   content
-  # end
+    annotations
+    |> Enum.reduce(content, fn
+      {"bold", true}, acc -> "<strong>#{acc}</strong>"
+      {"italic", true}, acc -> "<em>#{acc}</em>"
+      {"strikethrough", true}, acc -> "<del>#{acc}</del>"
+      {"underline", true}, acc -> "<u>#{acc}</u>"
+      {"code", true}, acc -> "<code>#{acc}</code>"
+      {"color", "default"}, acc -> acc
+      _, acc -> acc
+    end)
+  end
 end
