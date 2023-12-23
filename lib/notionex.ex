9bb01e8b3c441347f8348610a4dd1d545f71a426
Adarsh Menon <adarsh1021@gmail.com>
2023-12-23 23:08:43 +0530

    Run formatter and new context function to render by page id

--- lib/notionex.ex
+++ lib/notionex.ex
@@ -1,10 +1,16 @@
 defmodule Notionex do
   alias Notionex.Object
 
+  def render_block(page_id, renderer \\ Notionex.Renderer.HTML.Block, opts \\ %{})
+      when is_binary(page_id) do
+    Notionex.API.Blocks.list(page_id)
+    |> render_block(renderer, opts)
+  end
+
   def render_block(
         %Object.List{object: "list", type: "block"} = blocks,
-        renderer \\ Notionex.Renderer.HTML.Block,
-        opts \\ %{}
+        renderer,
+        opts
       ) do
     apply(renderer, :blocks, [blocks, opts])
   end
--- lib/notionex/renderer/utils.ex
+++ lib/notionex/renderer/utils.ex
@@ -3,25 +3,24 @@ defmodule Notionex.Renderer.Utils do
 
   # Current and prev blocks are numbered_list_items
   def update_numbered_list_item_number(
-         %Object.Block{type: "numbered_list_item"} = block,
-         %Object.Block{
-           type: "numbered_list_item",
-           numbered_list_item_number: numbered_list_item_number
-         } = _prev_block
-       ) do
+        %Object.Block{type: "numbered_list_item"} = block,
+        %Object.Block{
+          type: "numbered_list_item",
+          numbered_list_item_number: numbered_list_item_number
+        } = _prev_block
+      ) do
     block
     |> Map.put(:numbered_list_item_number, numbered_list_item_number + 1)
   end
 
   # Current block is numbered_list_item, prev block is not
   def update_numbered_list_item_number(
-         %Object.Block{type: "numbered_list_item"} = block,
-         _prev_block
-       ) do
+        %Object.Block{type: "numbered_list_item"} = block,
+        _prev_block
+      ) do
     block
     |> Map.put(:numbered_list_item_number, 1)
   end
 
   def update_numbered_list_item_number(block, prev_block), do: block
-
 end
