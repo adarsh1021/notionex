dd35bc028dfbc91ebb86403dd9120fd1e5aaf3f3
Adarsh Menon <adarsh1021@gmail.com>
2023-12-26 17:38:59 +0530

    Context function to render

--- lib/notionex.ex
+++ lib/notionex.ex
@@ -2,21 +2,13 @@ defmodule Notionex do
   # alias Notionex.Object
   alias Notionex.API
 
-  def request(%API.Request{} = request, endpoint, opts \\ []) do
-    apply(API, endpoint, [request, opts])
+  def request(request_params, endpoint, opts \\ []) do
+    apply(API, endpoint, [request_params, opts])
   end
 
-  # def render_block(page_id, renderer \\ Notionex.Renderer.HTML.Block, opts \\ %{})
-  #     when is_binary(page_id) do
-  #   Notionex.API.Blocks.list(page_id)
-  #   |> render_block(renderer, opts)
-  # end
-
-  # def render_block(
-  #       %Object.List{object: "list", type: "block"} = blocks,
-  #       renderer,
-  #       opts
-  #     ) do
-  #   apply(renderer, :blocks, [blocks, opts])
-  # end
+  def render_page(page_id, opts \\ []) do
+    %{block_id: page_id}
+    |> request(:retrieve_block_children, opts)
+    |> Notionex.Renderer.HTMLRenderer.render_block()
+  end
 end
